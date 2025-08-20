dd38eb43d9209d31677ee13596f7aa4dc6e155c3
Callum Kloos <callumkloos@gmail.com>
2025-08-20 12:35:19 +0100

    api token generation added

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :game_master_core, :scopes,
+  user: [
+    default: true,
+    module: GameMasterCore.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: GameMasterCore.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :game_master_core,
   ecto_repos: [GameMasterCore.Repo],
   generators: [timestamp_type: :utc_datetime]
