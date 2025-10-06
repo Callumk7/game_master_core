eff559da88d420b41291f6e6fd1508feb2db2624
Callum Kloos <callumkloos@gmail.com>
2025-10-06 17:35:01 +0100

    actually fixed: scoping issue for members

--- lib/game_master_core/helpers.ex
+++ lib/game_master_core/helpers.ex
@@ -7,7 +7,7 @@ defmodule GameMasterCore.Helpers do
 
   def get_scoped_character(scope, character_id) do
     try do
-      character = Characters.get_character!(scope, character_id)
+      character = Characters.get_character_for_game!(scope, character_id)
       {:ok, character}
     rescue
       Ecto.NoResultsError -> {:error, :character_not_found}
@@ -16,7 +16,7 @@ defmodule GameMasterCore.Helpers do
 
   def get_scoped_note(scope, note_id) do
     try do
-      note = Notes.get_note!(scope, note_id)
+      note = Notes.get_note_for_game!(scope, note_id)
       {:ok, note}
     rescue
       Ecto.NoResultsError -> {:error, :note_not_found}
@@ -25,7 +25,7 @@ defmodule GameMasterCore.Helpers do
 
   def get_scoped_faction(scope, faction_id) do
     try do
-      faction = Factions.get_faction!(scope, faction_id)
+      faction = Factions.get_faction_for_game!(scope, faction_id)
       {:ok, faction}
     rescue
       Ecto.NoResultsError -> {:error, :faction_not_found}
@@ -34,7 +34,7 @@ defmodule GameMasterCore.Helpers do
 
   def get_scoped_location(scope, location_id) do
     try do
-      location = Locations.get_location!(scope, location_id)
+      location = Locations.get_location_for_game!(scope, location_id)
       {:ok, location}
     rescue
       Ecto.NoResultsError -> {:error, :location_not_found}
@@ -43,7 +43,7 @@ defmodule GameMasterCore.Helpers do
 
   def get_scoped_quest(scope, quest_id) do
     try do
-      quest = Quests.get_quest!(scope, quest_id)
+      quest = Quests.get_quest_for_game!(scope, quest_id)
       {:ok, quest}
     rescue
       Ecto.NoResultsError -> {:error, :quest_not_found}
